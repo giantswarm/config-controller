637872dcaebe2abb2f717b316c8aec089e8e8177
kubasobon <wtty.fool@gmail.com>
2020-11-09 11:17:26 +0100

    update flag name

--- cmd/generate/flag.go
+++ cmd/generate/flag.go
@@ -6,24 +6,24 @@ import (
 )
 
 const (
-	flagApp          = "app"
-	flagDir          = "dir"
-	flagInstallation = "installation"
-	flagVersion      = "version"
+	flagApp           = "app"
+	flagDir           = "dir"
+	flagInstallation  = "installation"
+	flagConfigVersion = "config-version"
 )
 
 type flag struct {
-	App          string
-	Dir          string
-	Installation string
-	Version      string
+	App           string
+	Dir           string
+	Installation  string
+	ConfigVersion string
 }
 
 func (f *flag) Init(cmd *cobra.Command) {
 	cmd.Flags().StringVar(&f.App, flagApp, "", `Name of an application to generate the config for (e.g. "kvm-operator").`)
 	cmd.Flags().StringVar(&f.Dir, flagDir, "", "Directory containing configuration. If empty, contents of giantswarm/config will be used.")
 	cmd.Flags().StringVar(&f.Installation, flagInstallation, "", `Installation codename (e.g. "gauss").`)
-	cmd.Flags().StringVar(&f.Version, flagVersion, "", `Version of config to use for generation (e.g. "v2.3.19").`)
+	cmd.Flags().StringVar(&f.ConfigVersion, flagConfigVersion, "", `Version of config to use for generation (e.g. "v2.3.19").`)
 }
 
 func (f *flag) Validate() error {
@@ -33,8 +33,8 @@ func (f *flag) Validate() error {
 	if f.Installation == "" {
 		return microerror.Maskf(invalidFlagError, "--%s must not be empty", flagInstallation)
 	}
-	if f.Version == "" {
-		return microerror.Maskf(invalidFlagError, "--%s must not be empty", flagVersion)
+	if f.ConfigVersion == "" {
+		return microerror.Maskf(invalidFlagError, "--%s must not be empty", flagConfigVersion)
 	}
 
 	return nil

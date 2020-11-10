5817ec81e9fede79bddce63ca7e6324984d5064a
kubasobon <wtty.fool@gmail.com>
2020-11-10 09:42:40 +0100

    change flag to Branch

--- cmd/generate/flag.go
+++ cmd/generate/flag.go
@@ -7,21 +7,21 @@ import (
 
 const (
 	flagApp           = "app"
-	flagDir           = "dir"
+	flagBranch        = "branch"
 	flagInstallation  = "installation"
 	flagConfigVersion = "config-version"
 )
 
 type flag struct {
 	App           string
-	Dir           string
+	Branch        string
 	Installation  string
 	ConfigVersion string
 }
 
 func (f *flag) Init(cmd *cobra.Command) {
 	cmd.Flags().StringVar(&f.App, flagApp, "", `Name of an application to generate the config for (e.g. "kvm-operator").`)
-	cmd.Flags().StringVar(&f.Dir, flagDir, "", "Directory containing configuration. If empty, contents of giantswarm/config will be used.")
+	cmd.Flags().StringVar(&f.Branch, flagBranch, "", "Branch of giantswarm/config used to generate configuraton.")
 	cmd.Flags().StringVar(&f.Installation, flagInstallation, "", `Installation codename (e.g. "gauss").`)
 	cmd.Flags().StringVar(&f.ConfigVersion, flagConfigVersion, "", `Version of config to use for generation (e.g. "v2.3.19").`)
 }
@@ -33,7 +33,7 @@ func (f *flag) Validate() error {
 	if f.Installation == "" {
 		return microerror.Maskf(invalidFlagError, "--%s must not be empty", flagInstallation)
 	}
-	if f.ConfigVersion == "" {
+	if f.ConfigVersion == "" && f.Branch == "" {
 		return microerror.Maskf(invalidFlagError, "--%s must not be empty", flagConfigVersion)
 	}
 

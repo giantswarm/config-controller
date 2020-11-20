8e539c5e7e72fb1be660a418b0c7924fb107ed62
kubasobon <wtty.fool@gmail.com>
2020-11-20 12:33:30 +0100

    overwrite runner

--- cmd/generate/runner.go
+++ cmd/generate/runner.go
@@ -5,11 +5,11 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/ghodss/yaml"
 	"github.com/giantswarm/microerror"
 	"github.com/giantswarm/micrologger"
 	"github.com/spf13/cobra"
 
-	"github.com/giantswarm/config-controller/pkg/decrypt"
 	"github.com/giantswarm/config-controller/pkg/generator"
 	"github.com/giantswarm/config-controller/pkg/github"
 )
@@ -51,6 +51,7 @@ func (r *runner) run(ctx context.Context, cmd *cobra.Command, args []string) err
 	}
 
 	var store generator.Filesystem
+	var ref string
 	if r.flag.ConfigVersion != "" {
 		tag, err := gh.GetLatestTag(ctx, owner, repo, r.flag.ConfigVersion)
 		if err != nil {
@@ -61,11 +62,15 @@ func (r *runner) run(ctx context.Context, cmd *cobra.Command, args []string) err
 		if err != nil {
 			return microerror.Mask(err)
 		}
+
+		ref = tag
 	} else if r.flag.Branch != "" {
 		store, err = gh.GetFilesByBranch(ctx, owner, repo, r.flag.Branch)
 		if err != nil {
 			return microerror.Mask(err)
 		}
+
+		ref = r.flag.Branch
 	}
 
 	gen, err := generator.New(&generator.Config{
@@ -75,43 +80,24 @@ func (r *runner) run(ctx context.Context, cmd *cobra.Command, args []string) err
 		return microerror.Mask(err)
 	}
 
-	configmap, secrets, err := gen.GenerateConfig(r.flag.Installation, r.flag.App)
+	configmap, secrets, err := gen.GenerateConfig(r.flag.Installation, r.flag.App, ref)
 	if err != nil {
 		return microerror.Mask(err)
 	}
 
-	// TODO KUBA: clean up below
-
-	fmt.Fprintf(r.stdout, "Creating vault client using opsctl\n")
-
-	vaultClient, err := createVaultClientUsingOpsctl(ctx, r.flag.GitHubToken, r.flag.Installation)
+	fmt.Println("---")
+	out, err := yaml.Marshal(configmap)
 	if err != nil {
 		return microerror.Mask(err)
 	}
+	fmt.Printf(string(out) + "\n")
 
-	var decrypter *decrypt.Decrypter
-	{
-		c := decrypt.DecrypterConfig{
-			VaultClient: vaultClient,
-		}
-
-		decrypter, err = decrypt.New(c)
-		if err != nil {
-			return microerror.Mask(err)
-		}
-
-	}
-
-	if len(args) != 1 {
-		fmt.Fprintf(r.stderr, "Error: Expected the first argument to encrypted blob")
-	}
-
-	decrypted, err := decrypter.Decrypt(ctx, []byte(args[0]))
+	fmt.Println("---")
+	out, err = yaml.Marshal(secrets)
 	if err != nil {
 		return microerror.Mask(err)
 	}
-
-	fmt.Fprintf(r.stdout, "Decrypted: %s\n", decrypted)
+	fmt.Printf(string(out) + "\n")
 
 	return nil
 }

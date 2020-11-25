68d976158f99977d0b3976d6a15e9f2bc668bed0
Paweł Kopiczko <kopiczko@gmail.com>
2020-11-25 13:22:35 +0000

    Reconcile only labelled App CRs

---
+++ pkg/label/label.go
@@ -0,0 +1,34 @@
+package label
+
+import (
+	"github.com/giantswarm/config-controller/pkg/project"
+	"github.com/giantswarm/operatorkit/v4/pkg/controller"
+)
+
+const (
+	version = "config-controller.giantswarm.io/version"
+)
+
+func AppVersionSelector(unique bool) controller.Selector {
+	return controller.NewSelector(func(labels controller.Labels) bool {
+		if !labels.Has(version) {
+			return false
+		}
+		if labels.Get(version) == getProjectVersion(unique) {
+			return true
+		}
+
+		return false
+	})
+}
+
+func getProjectVersion(unique bool) string {
+	if unique {
+		// When config-controller is deployed as a unique app it only
+		// processes control plane app CRs. These CRs always have the
+		// version label config-controller.giantswarm.io/version: 0.0.0
+		return project.AppControlPlaneVersion()
+	} else {
+		return project.Version()
+	}
+}

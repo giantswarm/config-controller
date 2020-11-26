ba5bf8e0140e52297d71ea804b8f60dbee436631
kubasobon <wtty.fool@gmail.com>
2020-11-26 11:04:57 +0100

    remove unused imports

--- service/controller/resource/values/delete.go
+++ service/controller/resource/values/delete.go
@@ -6,8 +6,6 @@ import (
 
 	"github.com/giantswarm/microerror"
 	corev1 "k8s.io/api/core/v1"
-	apierrors "k8s.io/apimachinery/pkg/api/errors"
-	"k8s.io/apimachinery/pkg/labels"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	"github.com/giantswarm/config-controller/pkg/generator/key"

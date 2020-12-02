567a72a0ef80685182f95a1989d0807d78b47b3c
kubasobon <wtty.fool@gmail.com>
2020-12-02 14:18:08 +0100

    update existing configmap/secret

--- service/controller/resource/values/create.go
+++ service/controller/resource/values/create.go
@@ -34,20 +34,32 @@ func (r *Resource) EnsureCreated(ctx context.Context, obj interface{}) error {
 	r.logger.Debugf(ctx, "creating configmap %s/%s", configmap.Namespace, configmap.Name)
 	err = r.k8sClient.CtrlClient().Create(ctx, configmap)
 	if apierrors.IsAlreadyExists(err) {
-		r.logger.Debugf(ctx, "configmap %s/%s already exists", configmap.Namespace, configmap.Name)
+		r.logger.Debugf(ctx, "updating configmap %s/%s already exists", configmap.Namespace, configmap.Name)
+		err = r.k8sClient.CtrlClient().Update(ctx, configmap)
+		if err != nil {
+			return microerror.Mask(err)
+		}
+		r.logger.Debugf(ctx, "updated configmap %s/%s already exists", configmap.Namespace, configmap.Name)
 	} else if err != nil {
 		return microerror.Mask(err)
+	} else {
+		r.logger.Debugf(ctx, "created configmap %s/%s", configmap.Namespace, configmap.Name)
 	}
-	r.logger.Debugf(ctx, "created configmap %s/%s", configmap.Namespace, configmap.Name)
 
 	r.logger.Debugf(ctx, "creating secret %s/%s", secret.Namespace, secret.Name)
 	err = r.k8sClient.CtrlClient().Create(ctx, secret)
 	if apierrors.IsAlreadyExists(err) {
-		r.logger.Debugf(ctx, "secret %s/%s already exists", secret.Namespace, secret.Name)
+		r.logger.Debugf(ctx, "updating secret %s/%s already exists", secret.Namespace, secret.Name)
+		err = r.k8sClient.CtrlClient().Update(ctx, secret)
+		if err != nil {
+			return microerror.Mask(err)
+		}
+		r.logger.Debugf(ctx, "updated secret %s/%s already exists", secret.Namespace, secret.Name)
 	} else if err != nil {
 		return microerror.Mask(err)
+	} else {
+		r.logger.Debugf(ctx, "created secret %s/%s", secret.Namespace, secret.Name)
 	}
-	r.logger.Debugf(ctx, "created secret %s/%s", secret.Namespace, secret.Name)
 
 	r.logger.Debugf(ctx, "updating App CR %#q with configmap and secret details", app.Name)
 	app.Spec.Config.ConfigMap = v1alpha1.AppSpecConfigConfigMap{

859305bba9b294bdfab7cedfe4c023e40d5a27f1
kubasobon <wtty.fool@gmail.com>
2020-12-03 15:50:04 +0100

    smart context cancelling

--- service/controller/resource/values/create.go
+++ service/controller/resource/values/create.go
@@ -6,6 +6,7 @@ import (
 	"github.com/giantswarm/apiextensions/v3/pkg/annotation"
 	"github.com/giantswarm/apiextensions/v3/pkg/apis/application/v1alpha1"
 	"github.com/giantswarm/microerror"
+	"github.com/giantswarm/operatorkit/v4/pkg/controller/context/resourcecanceledcontext"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 
 	controllerkey "github.com/giantswarm/config-controller/service/controller/key"
@@ -17,10 +18,18 @@ func (r *Resource) EnsureCreated(ctx context.Context, obj interface{}) error {
 		return microerror.Mask(err)
 	}
 
+	if app.GetDeletionTimestamp() != nil {
+		r.logger.Debugf(ctx, "App CR %q is has deletion timestamp", app.Name)
+		r.logger.Debugf(ctx, "cancelling resource")
+		resourcecanceledcontext.SetCanceled(ctx)
+		return nil
+	}
+
 	configVersion, ok := app.GetAnnotations()[annotation.ConfigVersion]
 	if !ok {
 		r.logger.Debugf(ctx, "App CR %q is missing %q annotation", app.Name, annotation.ConfigVersion)
 		r.logger.Debugf(ctx, "cancelling resource")
+		resourcecanceledcontext.SetCanceled(ctx)
 		return nil
 	}
 
@@ -55,20 +64,22 @@ func (r *Resource) EnsureCreated(ctx context.Context, obj interface{}) error {
 	}
 	r.logger.Debugf(ctx, "ensured secret %s/%s", secret.Namespace, secret.Name)
 
-	r.logger.Debugf(ctx, "updating App CR %#q with configmap and secret details", app.Name)
-	app.Spec.Config.ConfigMap = v1alpha1.AppSpecConfigConfigMap{
-		Namespace: configmap.Namespace,
-		Name:      configmap.Name,
+	if app.Spec.Config.ConfigMap.Name == "" || app.Spec.Config.Secret.Name == "" {
+		r.logger.Debugf(ctx, "updating App CR %#q with configmap and secret details", app.Name)
+		app.Spec.Config.ConfigMap = v1alpha1.AppSpecConfigConfigMap{
+			Namespace: configmap.Namespace,
+			Name:      configmap.Name,
+		}
+		app.Spec.Config.Secret = v1alpha1.AppSpecConfigSecret{
+			Namespace: secret.Namespace,
+			Name:      secret.Name,
+		}
+		err = r.k8sClient.CtrlClient().Update(ctx, &app)
+		if err != nil {
+			return microerror.Mask(err)
+		}
+		r.logger.Debugf(ctx, "updated App CR %#q with configmap and secret details", app.Name)
 	}
-	app.Spec.Config.Secret = v1alpha1.AppSpecConfigSecret{
-		Namespace: secret.Namespace,
-		Name:      secret.Name,
-	}
-	err = r.k8sClient.CtrlClient().Update(ctx, &app)
-	if err != nil {
-		return microerror.Mask(err)
-	}
-	r.logger.Debugf(ctx, "updated App CR %#q with configmap and secret details", app.Name)
 
 	return nil
 }

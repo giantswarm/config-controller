4bad06784c1e887ab0d74bbd4f29bafaf0e7669c
GitHub Action <action@github.com>
2021-11-29 22:38:36 +0000

    Bump version to 0.5.1-dev

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "config-controller"
 	source      = "https://github.com/giantswarm/config-controller"
-	version     = "0.5.0"
+	version     = "0.5.1-dev"
 )
 
 func Description() string {
